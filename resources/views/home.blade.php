31a461407484c23a13b952aa7229262ac46e0bbe
D-Undefined <danielabdi12@gmail.com>
2020-09-30 19:30:10 +0200

    CSS werkt niet. Begin gemaakt aan de home page.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+@extends('layout/layout')
+
+@section('content')
+    <div class="navigation">
+        <nav>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#login">Log in</a></li>
+            </ul>
+        </nav>
+    </div>
+
+    <div class="options">
+        <a href="#home">Look for a fight</a>
+        <a href="#home">Register</a>
+    </div>
+    @endsection
+
+
+
+
