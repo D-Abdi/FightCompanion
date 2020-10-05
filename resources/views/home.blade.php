e00d5dd6f3d2868550c989b0a6048b5c6d528342
D-Undefined <danielabdi12@gmail.com>
2020-10-05 15:34:12 +0200

    + Database opgezet + Log in werkt + Uitloggen werkt ook + fighter model aangemaakt
    - Nog geen radio buttons aangemaakt voor de selectie tussen admin en fighter

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,21 +1,23 @@
-@extends('layout/layout')
+@extends('layouts.app')
 
 @section('content')
-    <div class="navigation">
-        <nav>
-            <ul>
-                <li><a href="#home">Home</a></li>
-                <li><a href="#login">Log in</a></li>
-            </ul>
-        </nav>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
+        </div>
     </div>
-
-    <div class="options">
-        <a href="#home">Look for a fight</a>
-        <a href="#home">Register</a>
-    </div>
-    @endsection
-
-
-
-
+</div>
+@endsection
