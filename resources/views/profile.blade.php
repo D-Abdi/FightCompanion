2164e6758f87822aedb95db13d78a8a857a9c75e
D-Undefined <danielabdi12@gmail.com>
2020-10-10 14:09:01 +0200

    Persoonlijk profiel voorbereid. Moet nog connect worden met de database.

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -2,7 +2,35 @@
 
 @section('content')
   <div class="container">
-      <img src="css/images/profile.png" alt="profile picture" class="img-fluid">
+      <div class="profile-container">
+          <img src="css/images/profile.png" alt="profile picture" class="profile-picture"/>
+          <table class="profile-description-1">
+              <thead>
+              <tr>
+                  <th class="profile-description-text">Name</th>
+                  <th class="profile-description-text">Gym</th>
+                  <th class="profile-description-text">Discipline</th>
+                  <th class="profile-description-text">Weight</th>
+              </tr>
+              </thead>
+              <tbody>
+              <tr>
+                  <td class="profile-description-text">Daniel Abdi</td>
+                  <td class="profile-description-text">KR Boxing</td>
+                  <td class="profile-description-text">Boxing</td>
+                  <td class="profile-description-text">74</td>
+              </tr>
+              </tbody>
+          </table>
+      </div>
+      <div class="profile-container">
+          <h2>Past Sparrings</h2>
+          <img src="css/images/card.png" alt="profile picture" class="profile-card"/>
+          <img src="css/images/card.png" alt="profile picture" class="profile-card"/>
+          <img src="css/images/card.png" alt="profile picture" class="profile-card"/>
+          <img src="css/images/card.png" alt="profile picture" class="profile-card"/>
+      </div>
+      <a href="#" class="edit-profile btn btn-primary">Edit Profile</a>
   </div>
 
 @endsection
