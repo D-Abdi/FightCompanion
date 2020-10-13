4ca77ea2b00b22560c3024f344f279338c674e9b
D-Undefined <danielabdi12@gmail.com>
2020-10-13 17:41:51 +0200

    Alle gyms worden nu getoond op de /gym pagina. Afbeelding wordt alleen nog niet geladen. De url wordt letterlijk opgeslagen.

--- resources/views/gym/gym.blade.php
+++ resources/views/gym/gym.blade.php
@@ -2,6 +2,14 @@
 
 @section('content')
     <h1>Gym's</h1>
+    @foreach($gyms as $gym)
+        <li>
+            <h2>{{$gym->name}}</h2>
+            <p>{{$gym->phone_number}}</p>
+            <p>{{$gym->email}}</p>
+            <p>{{$gym->logo}}</p>
+        </li>
+    @endforeach
 
 
 @endsection
