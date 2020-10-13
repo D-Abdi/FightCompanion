55ff2474bbc6fbfb7a538ab2b7fd589a1fb27323
D-Undefined <danielabdi12@gmail.com>
2020-10-13 18:34:25 +0200

    Alle gyms worden nu getoond op de /gym pagina. Afbeelding wordt alleen nog niet geladen. Form can de gym/create is beetje gestyled. De url van de foto wordt nogsteeds letterlijk opgeslagen.

--- resources/views/gym/create-gym.blade.php
+++ resources/views/gym/create-gym.blade.php
@@ -1,67 +1,70 @@
 @extends('layouts.app')
 
 @section('content')
-    <div id="wrapper">
-        <div id="page" class="container">
-        <h1 class="heading has-text-weight-bold is-size-4">Create a gym</h1>
+    <div id="container" style="font-family:Verdana,sans-serif">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <h1 class="" style="text-align:center;font-size: x-large">Create a gym</h1>
+                    <div class="card-body">
+                        <form  method="post" action="/gym" enctype="multipart/form-data">
+                            @csrf
+                            <div class="form-group row">
+                                <label class="col-md-4 col-form-label text-md-right" for="titel">Gym Name</label>
 
-        <form  method="post" action="/gym">
-            @csrf
-            <div class="field">
-                <label class="label" for="titel">Name</label>
+                                <div class="col-md-6">
+                                    <input type="text" name="name" id="name" class="form-control">
+                                </div>
+                                </div>
 
-                <div class="control">
-                    <input type="text" name="name" id="name">
-                </div>
-            </div>
+                            <div class="form-group row">
+                                <label for="adres" class="col-md-4 col-form-label text-md-right">Adres & Number</label>
 
-            <div class="field">
-                <label class="label" for="adres">Adres</label>
+                                <div class="col-md-6">
+                                    <input type="text" name="adres" class="form-control" id="adres">
+                                </div>
+                            </div>
 
-                <div class="control">
-                    <input type="text" name="adres" id="adres">
-                </div>
-            </div>
+                            <div class="form-group row">
+                                <label class="col-md-4 col-form-label text-md-right" for="zip_code">Zip Code</label>
 
-            <div class="field">
-                <label class="label" for="zip_code">Zip Code</label>
+                                <div class="col-md-6">
+                                    <input type="text" name="zip_code" class="form-control" id="zip_code">
+                                </div>
+                            </div>
 
-                <div class="control">
-                    <input type="text" name="zip_code" id="zip_code">
-                </div>
-            </div>
+                            <div class="form-group row">
+                                <label class="col-md-4 col-form-label text-md-right" for="phone_number">Phone Number</label>
 
-            <div class="field">
-                <label class="label" for="phone_number">Phone Number</label>
+                                <div class="col-md-6">
+                                    <input type="number" name="phone_number"  class="form-control" id="phone_number">
+                                </div>
+                            </div>
 
-                <div class="control">
-                    <input type="number" name="phone_number" id="phone_number">
-                </div>
-            </div>
+                            <div class="form-group row">
+                                <label class="col-md-4 col-form-label text-md-right" for="email">Email</label>
 
-            <div class="field">
-                <label class="label" for="email">Email</label>
+                                <div class="col-md-6">
+                                    <input type="email" name="email"  class="form-control" id="email">
+                                </div>
+                            </div>
 
-                <div class="control">
-                    <input type="email" name="email" id="email">
-                </div>
-            </div>
+                            <div class="form-group row">
+                                <label class="col-md-4 col-form-label text-md-right" for="Logo">Upload Logo</label>
 
-            <div class="field">
-                <label class="label" for="Logo">Upload Logo</label>
+                                <div class="col-md-6">
+                                    <input type="file" name="logo" id="logo"  class="btn-light" accept="image/jpeg,image/png,image/jpg">
+                                </div>
+                            </div>
 
-                <div class="control">
-                    <input type="file" name="logo" id="logo" accept="image/jpeg,image/png,image/jpg">
-                </div>
-            </div>
-
-            <div class="field is-grouped">
-                <div class="control">
-                    <button class="button is-link" type="submit">Submit</button>
+                            <div class="form-group row mb-0">
+                                <div class="col-md-6 offset-md-4">
+                                    <button class="btn btn-primary btn-lg" type="submit">Submit</button>
                 </div>
             </div>
 
         </form>
+        </div>
+        </div>
         </div>
     </div>
 
