f9bf5ad80b62e85136ba334d217effc4636f6254
D-Undefined <danielabdi12@gmail.com>
2020-10-14 18:00:40 +0200

    Opmaak aangepast en de informatie op een betere manier weergegeven. Volgende stap is een in depth profiel maken van elke gym.

--- resources/views/gym/gym.blade.php
+++ resources/views/gym/gym.blade.php
@@ -1,15 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Gym's</h1>
-    @foreach($gyms as $gym)
-        <li>
-            <img src="{{asset('/storage/logos/'.$gym->logo)}}"/>
-            <h2>{{$gym->name}}</h2>
-            <p>{{$gym->phone_number}}</p>
-            <p>{{$gym->email}}</p>
-        </li>
-    @endforeach
+    <div class="container">
+        <h1>Gym's</h1>
+        @foreach($gyms as $gym)
+            <div class="gym-card">
+                <img src="{{asset('/storage/logos/'.$gym->logo)}}" id="gym-image" style="width: 100%;height: 150px"/>
+                <div class="gym-container">
+                    <h4>{{$gym->name}}</h4>
+                    <p>{{$gym->phone_number}}</p>
+                    <p>{{$gym->email}}</p>
+                </div>
+            </div>
+        @endforeach
 
+    </div>
 
 @endsection
